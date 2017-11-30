c27f7166ae88850c71e6899f504706a077a658c8
Leslie Johnson <bithits@gmail.com>
2017-11-30 09:30:06 -0600

    Added params to attempt cache. Not sure it works yet.

--- src/FetchHtmlText.js
+++ src/FetchHtmlText.js
@@ -4,9 +4,8 @@ import {langIndex} from './langIndex.js'
 
 export default function FetchHtmlText(props) {
   const lang=['EN','ES'] // Directory names.
-  let now = new Date().getTime()
   let node = <div className='loadText'></div>
-  fetch(`./${lang[langIndex()]}/${props.children[0]}.html?${now}`)
+  fetch(`./${lang[langIndex()]}/${props.children[0]}.html`,{cache: "no-cache, no-store, must-revalidate"})
     .then(resp => resp.text())
     .then(text => node.innerHTML = text)
   return(node)
