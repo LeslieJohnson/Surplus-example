000fbc2f00e8587d710baa81a6d735f2189bba82
Leslie Johnson <bithits@gmail.com>
2017-12-18 15:36:26 -0600

    Removed unneeded ModalBase import. Changed all components from <Component></Component to <Component/>

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ import Pictures from './Pictures.js'
 import News from './News.js'
 import Events from './Events.js'
 import Footer from './Footer.js'
-import ModalBase from './ModalBase'
 
 let view = S.root(() =>
   <div className='App'>
     <div className='main'>
-      <TopNav></TopNav>
-      <Header></Header>
-      <Intro></Intro>
-      <Pictures></Pictures>
-      <News></News>
-      <Events></Events>
-      <Footer></Footer>
+      <TopNav/>
+      <Header/>
+      <Intro/>
+      <Pictures/>
+      <News/>
+      <Events/>
+      <Footer/>
     </div>
   </div>
 )
