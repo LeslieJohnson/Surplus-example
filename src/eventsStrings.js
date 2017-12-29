7c88c5e5000f114847516d6e850580c9fbacd1fb
Leslie Johnson <bithits@gmail.com>
2017-12-29 11:47:00 -0600

    updated news and events

--- src/eventsStrings.js
+++ src/eventsStrings.js
@@ -1,5 +1,6 @@
 export const eventsStrings = {
-  obligation: ['Holy days of obligation','días santo de la obligación','December 24, 25','Diciembre 24, 25'],
+  epiphany: ['Epiphany Mass','masa de epifanía','January 5, 10AM','5 de Enero 10AM'],
+  blessings: ['Blessing of Sacramentals','bendición de los sacramentales','Jan 5-6','5-6 de Enero'],
   christmas: ['Christmas Schedule','Horario de Navidad','December 25','Diciembre 25'],
   bakesale: ['Bake Sale for Maidens','venta de pasteles para doncellas','December 24','Diciembre 24'],
   fformation: ['Faith formation begins','Comienza la formación de la fe','Every Wednesday 6:30 PM','Diciembre 4']
--- src/newsStrings.js
+++ src/newsStrings.js
@@ -1,7 +1,7 @@
 export const newsStrings = {
+  signup: ['signup for emails','registrarse para correos electrónicos','sheet in back of church','parte posterior de la iglesia'],
   jan1: ['January 1 not holy obligation','1 de enero no obligación sagrada','January 1, 7 PM','1 de Enero, 7 PM'],
   maidens: ['Maidens of the Altar Ceremony','Las Doncellas del Altar','December 8','Diciembre 8'],
   youth: ['Youth Group','Grupo de jovenes','Sundays 6-8 PM','Domingos 6-8 PM'],
   callingmen: ['Calling all men!','¡Llamando a todos los hombres!','January 5, 6:00 PM','5 de enero, 6:00 PM'],
-  baked: ['maidens need baked goods','las doncellas necesitan productos horneados','December 24','Diciembre 24']
 }
