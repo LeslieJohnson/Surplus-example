d0a7823e2a5069869d2825371a4bb25b393ff98a
Leslie Johnson <bithits@gmail.com>
2017-12-31 14:01:47 -0600

    Updated for Christmas week. Fixed date for Epiphany.

--- src/eventsStrings.js
+++ src/eventsStrings.js
@@ -1,7 +1,6 @@
 export const eventsStrings = {
-  epiphany: ['Epiphany Mass','masa de epifanía','January 5, 10AM','5 de Enero 10AM'],
+  epiphany: ['Epiphany Mass','masa de epifanía','January 6, 10AM','6 de Enero 10AM'],
   blessings: ['Blessing of Sacramentals','bendición de los sacramentales','Jan 5-6','5-6 de Enero'],
-  christmas: ['Christmas Schedule','Horario de Navidad','December 25','Diciembre 25'],
   bakesale: ['Bake Sale for Maidens','venta de pasteles para doncellas','December 24','Diciembre 24'],
   fformation: ['Faith formation begins','Comienza la formación de la fe','Every Wednesday 6:30 PM','Diciembre 4']
 }
