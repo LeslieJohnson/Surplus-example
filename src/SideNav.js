500504a315829067c2a06eedb5addf4c75d66613
Leslie Johnson <bithits@gmail.com>
2018-04-14 13:48:58 -0500

    Added JSX style object and removed old inline style string. Removed String.fromCharCode and replaced with '&times;' in text.

--- src/SideNav.js
+++ src/SideNav.js
@@ -7,28 +7,32 @@ const close = () => {
   displayState('none')
 }
 export default function SideNav () {
+  let divStyle = {
+    fontSize: '2em',
+    padding: '0 0 0 0.3em'
+  }
   let myside = <div className='sidenav'>
-    <div />
+  <div />
 
-    <MenuItem size='840px' >calendar</MenuItem>
-    <MenuItem size='370px' >readings</MenuItem>
-    <MenuItem size='60%' >pastors</MenuItem>
-    <div><hr /></div>
-    <MenuItem size='30%' >confession</MenuItem>
-    <MenuItem size='30%' >adoration</MenuItem>
-    <div><hr /></div>
-    <MenuItem size='60%' >sick</MenuItem>
-    <MenuItem size='60%' >troops</MenuItem>
-    <MenuItem size='40%' >vocations</MenuItem>
-    <div><hr /></div>
-    <MenuItem size='40%' >gift</MenuItem>
-    <MenuItem size='60%' >family</MenuItem>
-    <MenuItem size='60%' >prayrosary</MenuItem>
-    <div><hr /></div>
-    <MenuItem size='40%' >knights</MenuItem>
-    <MenuItem size='40%' >maidens</MenuItem>
-    <MenuItem size='40%' >firstfriday</MenuItem>
-    <div onClick={close} style='font-size: 2em; padding: 0 0 0 0.3em;'>{String.fromCharCode(215)}</div>
+  <MenuItem size='840px' >calendar</MenuItem>
+  <MenuItem size='370px' >readings</MenuItem>
+  <MenuItem size='60%' >pastors</MenuItem>
+  <div><hr /></div>
+  <MenuItem size='30%' >confession</MenuItem>
+  <MenuItem size='30%' >adoration</MenuItem>
+  <div><hr /></div>
+  <MenuItem size='60%' >sick</MenuItem>
+  <MenuItem size='60%' >troops</MenuItem>
+  <MenuItem size='40%' >vocations</MenuItem>
+  <div><hr /></div>
+  <MenuItem size='40%' >gift</MenuItem>
+  <MenuItem size='60%' >family</MenuItem>
+  <MenuItem size='60%' >prayrosary</MenuItem>
+  <div><hr /></div>
+  <MenuItem size='40%' >knights</MenuItem>
+  <MenuItem size='40%' >maidens</MenuItem>
+  <MenuItem size='40%' >firstfriday</MenuItem>
+  <div onClick={close} style={divStyle}>&times;</div>
   </div>
   myside.style.display = displayState()
   return (
