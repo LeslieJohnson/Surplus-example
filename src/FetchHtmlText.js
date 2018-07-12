138aec8f2f7f26cf9915a6cfbfd67cbb1bacd4e1
Leslie Johnson <bithits@gmail.com>
2018-07-12 17:57:45 -0500

    Added proper cache:"no-cache" parameter to fetch.

--- src/FetchHtmlText.js
+++ src/FetchHtmlText.js
@@ -5,7 +5,8 @@ import {langIndex} from './langIndex.js'
 export default function FetchHtmlText (props) {
   const lang = ['EN', 'ES'] // Directory names.
   let node = <div className='loadText' />
-  fetch(`./${lang[langIndex()]}/${props.children}.html`)
+  fetch(`./${lang[langIndex()]}/${props.children}.html`,
+        {cache:'no-cache'})
     .then(resp => resp.text())
     .then(text => node.innerHTML = text)
   return (node)
